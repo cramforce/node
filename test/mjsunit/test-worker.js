e98c824133399e03e24cd40118032d99f2a63e93
malubl <malubl@joose.alicenet>
2009-11-10 22:18:48 +0100

    Change indent to two spaces

--- test/mjsunit/test-worker.js
+++ test/mjsunit/test-worker.js
@@ -6,24 +6,24 @@ var Worker = require("worker").Worker;
 var worker = new Worker(__filename.replace("test-", "fixtures/"));
 
 worker.onmessage = function (msg) {
-    if(msg.input) {
-        assertEquals(msg.output, msg.input * 3);
-        sys.error("Good")
-    }
+  if(msg.input) {
+    assertEquals(msg.output, msg.input * 3);
+    sys.error("Good");
+  }
 }
 
 worker.addListener("message", function (msg) {
-    if(msg == "terminate") {
-        worker.terminate();
-    }
+  if(msg == "terminate") {
+    worker.terminate();
+  }
 })
 
 worker.postMessage({
-    input: 1
+  input: 1
 });
 worker.postMessage({
-    input: 2
+  input: 2
 });
 setTimeout(function () {
-    worker.postMessage("terminate");
-}, 1000)
+  worker.postMessage("terminate");
+}, 1000);
