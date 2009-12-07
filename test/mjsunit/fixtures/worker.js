5646e32db8cea66e4266d6eaf117f61418d105bd
Malte Ubl <malubl@Malte-Ubls-MacBook-Pro.local>
2009-12-07 21:39:44 +0100

    Fixed an inifite recursion bug. Added tests.

--- test/mjsunit/fixtures/worker.js
+++ test/mjsunit/fixtures/worker.js
@@ -10,6 +10,29 @@ var sys    = require("sys");
 var worker = require("worker").worker;
 
 worker.onmessage = function (msg) {
+    sys.error("OnMessage " +JSON.stringify(msg));
+    
+    if(msg.fib) {
+      worker.postMessage(fib(msg.fib*1));
+      return;
+    }
+    
+    if(msg.wait) {
+      setTimeout(function () {
+        worker.postMessage("Waited")
+      }, 1000)
+      return;
+    }
+    
+    if(msg.error) {
+      throw("ErrorMarker");
+    }
+    
     msg.output = msg.input * 3;
     worker.postMessage(msg);
 };
+
+
+function fib(n) {
+  return n < 2 ? n : fib(n-1)+fib(n-2);
+}
--- test/mjsunit/test-worker.js
+++ test/mjsunit/test-worker.js
@@ -5,20 +5,18 @@ var Worker = require("worker").Worker;
 
 process.ENV["NODE_PATH"] = process.libDir;
 
-var worker = new Worker(__filename.replace("test-", "fixtures/"));
+function makeWorker () {
+  return new Worker(__filename.replace("test-", "fixtures/"));
+}
+
+var worker = makeWorker();
 
 worker.onmessage = function (msg) {
   if (msg.input) {
-    assertEquals(msg.output, msg.input * 3);
+    assert.equal(msg.output, msg.input * 3);
     sys.error("Good");
   }
-}
-
-worker.addListener("message", function (msg) {
-  if (msg == "terminate") {
-    worker.terminate();
-  }
-})
+};
 
 worker.postMessage({
   input: 1
@@ -26,6 +24,60 @@ worker.postMessage({
 worker.postMessage({
   input: 2
 });
+worker.postMessage({
+  input: 3
+});
+worker.postMessage({
+  input: 4
+});
+
 setTimeout(function () {
-  worker.postMessage("terminate");
+  worker.terminate();
+}, 2000);
+
+setTimeout(function () {
+  setTimeout(function () {
+    var w2 = makeWorker();
+    w2.postMessage({
+      error: true
+    });
+    w2.addListener("error", function () {
+      sys.error("test");
+      assert.ok(true, "Received expected error");
+      w2.terminate();
+    });
+    w2.addListener("message", function () {
+      assert.ok(false, "Wanted an error, but got a message");
+      w2.terminate();
+    });
+    worker.postMessage({
+      input: 8
+    });
+  }, 1000);
+  worker.terminate()
 }, 1000);
+
+var fibWorker = makeWorker();
+fibWorker.postMessage({
+  fib: 41
+});
+var ret = false;
+fibWorker.addListener("message", function (fib) {
+  sys.error("Fib "+fib);
+  ret = true;
+  assert.ok(fib === 165580141, "Worker can do long running stuff.")
+  fibWorker.terminate();
+});
+for(var i = 0; i < 100; ++i) {
+  // counting;
+}
+assert.ok(!ret, "Can do work while background spins");
+
+var waitWorker = makeWorker();
+waitWorker.postMessage({
+  wait: true
+});
+waitWorker.addListener("message", function () {
+  assert.ok(true, "Worker response can be async.")
+  waitWorker.terminate();
+});
